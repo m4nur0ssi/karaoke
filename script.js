fddcb7cd198b3cc5cfb3a3d55396e7e940270e16
m4nur0ssi <m4nu.r0ssi@gmail.com>
2026-02-24 12:09:56 +0100

    Sync script.js (v53.9)

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const checkFirebase = () => {
     return { ok: true, msg: 'Firebase OK' };
 };
 
-logDebug('Script loaded v2026_v53.8');
+logDebug('Script loaded v2026_v53.9');
 
 // Tentative de synchronisation immédiate
 setTimeout(() => {
